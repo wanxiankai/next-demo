3ecf5d12481795deb9f614701b42357aed7d171f
wanxiankai <xiankai.wan@gmail.com>
2025-11-06 13:52:00 +0800

    feat: define the constants for path aliases

---
+++ src/constants/common.ts
@@ -0,0 +1,4 @@
+export const INTL_SUPPORTED_LOCALES =  ["en", "ko", "fr", "de", "ja"];
+export const INTL_DEFAULT_LOCALE = 'en';
+export const CN_SUPPORTED_LOCALES = ['zh-CN'];
+export const CN_DEFAULT_LOCALE = 'zh-CN';
---
+++ src/constants/index.ts
@@ -0,0 +1,5 @@
+import * as COMMON from './common';
+
+export {
+  COMMON,
+};
